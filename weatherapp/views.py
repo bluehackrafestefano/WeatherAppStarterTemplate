ad0ba02648a764956b25eaba10f94c8a07c99c3b
rafe <rafe@clarusway.com>
2022-06-07 20:29:12 +0200

    last version added delete feature

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,4 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from decouple import config
+import requests
+from pprint import pprint
+from .models import City
+from django.contrib import messages
 
-def home(request):    
-    return render(request, "weatherapp/home.html")
+
+def home(request):
+    API_key=config('API_KEY')
+    u_city = request.GET.get('u_city')
+    
+    if u_city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_key}&units=metric"
+        r = requests.get(url)
+        pprint(r.ok)
+        if r.ok:
+            content = r.json()
+            r_city = content.get('name')
+            if City.objects.filter(name=r_city):
+                messages.warning(request, message='The city exists!')
+            else:
+                City.objects.create(name=r_city)
+                messages.success(request, message='City created!')
+        else:
+            messages.error(request, message='City can not found!')
+        return redirect('home')
+    
+    city_data = []
+    cities = City.objects.all()
+    for city in cities:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}&units=metric"
+        r = requests.get(url)
+        content = r.json()
+        data = {
+            'city': city,
+            'temp': content['main']['temp'],
+            'desc': content['weather'][0]['description'],
+            'icon': content['weather'][0]['icon'],
+        }
+        city_data.append(data)
+    context = {
+        'city_data': city_data,
+    }
+    return render(request, "weatherapp/home.html", context)
+
+def delete(request, id):
+    city = get_object_or_404(City, id=id)
+    city.delete()
+    messages.success(request, message='City deleted!')
+    return redirect('home')
