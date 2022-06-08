b416b3caad2671c4ea1fec3e15120ba66481a047
rafe <rafe@clarusway.com>
2022-06-08 20:47:26 +0200

    added logic

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,11 +1,27 @@
+from weatherapp.models import City
 from django.shortcuts import render
 from decouple import config
 import requests
 from pprint import pprint
+from django.contrib import messages
 
 def home(request):
     API_key = config('API_KEY')
-    city = 'Yozgat'
+    u_city = request.GET.get('name')
+    if u_city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_key}&units=metric"
+        response = requests.get(url)
+        if response.ok:
+            content = response.json()
+            r_city = content['name']
+            if City.objects.filter(name=r_city):
+                messages.warning(request, 'City already exists!')
+            else:
+                City.objects.create(name=r_city)
+                messages.success(request, 'City created!')
+        else:
+            messages.error(request, 'There is no city!')
+    
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}&units=metric"
     response = requests.get(url)
     content = response.json()
