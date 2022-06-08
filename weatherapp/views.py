43c04e3c93cabd6f03a15bd6eb5118c8d59c269c
rafe <rafe@clarusway.com>
2022-06-08 20:26:03 +0200

    last version

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint
 
 def home(request):
-    return render(request, 'weatherapp/home.html')
+    API_key = config('API_KEY')
+    city = 'Yozgat'
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}&units=metric"
+    response = requests.get(url)
+    content = response.json()
+    
+    context = {
+        'city': content['name'],
+        'temp': content['main']['temp'],
+        'icon' : content['weather'][0]['icon'],
+        'desc' : content['weather'][0]['description'],
+    }
+    return render(request, 'weatherapp/home.html', context)
